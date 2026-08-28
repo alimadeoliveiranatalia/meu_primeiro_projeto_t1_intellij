c6a9752aa3d16cf266bf6fc60912fc3aca4365e3
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2026-08-28 14:47:38 -0300

    feat: Add Algoritmo de soma de dois numeros

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args){
-        System.out.print("ola");
+        // Algoritmo para somar duas notas
+        int nota1 = 3;
+        int nota2 = 6;
+        System.out.print(nota1 + nota2);
     }
 }
